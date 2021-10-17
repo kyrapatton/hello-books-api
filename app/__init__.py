c3b4996ab2af19b9228a135f54249401f0529f4f
Kyra <kyra.ptn@gmail.com>
2021-10-16 20:50:07 -0700

    two initial endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,10 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #register blueprint -- necessary everytime new blueprint is instantiated
+    #registering = tell the app to use endpoints from hello_world_bp for its routing
+    from .routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
+
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+#arg1: string used to identify this Blueprint from the Flask server logs
+#arg2: uses __name__ to figure where the home folder is
+
+#create an endpoint
+@hello_world_bp.route('/hello-world', methods=["GET"])
+def get_hello_world():
+    my_response_body = "Hello, World!"
+    return my_response_body
+
+@hello_world_bp.route('/hello-world/JSON', methods = ['GET'])
+def hello_world_json():
+    return {
+        "name": "Kyra",
+        "message" : "Heya",
+        "hobbies" : ["coding", "writing", "reading"]
+    }, 200
+    
